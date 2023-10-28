78c5dc76f8469811b0e320dddc03c37e57c7586b
Yrnar <ernar030913@gmail.com>
2023-10-28 15:52:30 +0600

    prof4

--- mid/src/Components/Profile/Profile.jsx
+++ mid/src/Components/Profile/Profile.jsx
@@ -8,7 +8,7 @@ const Profile = () => {
         <img className="im" src='https://png.pngtree.com/png-vector/20191110/ourmid/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg' alt='Profile pic'></img>
         <div className="data">Yernar Zhilkibaev</div>
         <div className="data">+7 705 777 77 77</div>
-        <div className="data">Almaty</div>
+        <div className="data">Almaty, Kazakhstan</div>
     </div>
   )
 }
