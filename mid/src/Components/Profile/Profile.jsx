143ea1ef7f49c5845e90a73b858bce42181afc9a
Yrnar <ernar030913@gmail.com>
2023-10-28 15:50:38 +0600

    prof2

--- mid/src/Components/Profile/Profile.jsx
+++ mid/src/Components/Profile/Profile.jsx
@@ -4,8 +4,11 @@ import "./Profile.css";
 const Profile = () => {
   return (
     <div>
-        <h1>Your profile</h1>
+        <h1>YOUR PROFILE</h1>
         <img className="im" src='https://png.pngtree.com/png-vector/20191110/ourmid/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg' alt='Profile pic'></img>
+        <div className="data">Yernar Zhilkibaev</div>
+        <div className="data">+7 705 777 77 77</div>
+        <div className="data">Almaty</div>
     </div>
   )
 }
